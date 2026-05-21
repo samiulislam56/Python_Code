703899f91106d163b558d91ef7655be8fadb57e8
Samiul islam sami <samiulislam07186@gmail.com>
2026-05-21 15:31:33 +0600

    Add files via upload

---
+++ Data_types.py
@@ -0,0 +1,8 @@
+name = "Sami" # This is a str type variable
+print(name)
+
+name1 = "Sami" # This is a str type variable
+print(name1)
+
+name_1 = "Sami" # This is a str type variable
+print(name_1)
