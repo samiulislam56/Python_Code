bc989557976cdb271a59d07288efe01c3b6e1432
Samiul islam sami <samiulislam07186@gmail.com>
2026-05-21 15:47:18 +0600

    Add files via upload

---
+++ Number_type_data.py
@@ -0,0 +1,18 @@
+# int type data
+number = 900
+print(number)
+print(type(number))
+
+# float type data
+float_number = 40.2
+print(float_number)
+print(type(float_number))
+
+#complex type data
+complex_number = 20j
+print(complex_number)
+print(type(complex_number))
+
+complex_number1 = 20 + 30j
+print(complex_number1)
+print(type(complex_number1))
