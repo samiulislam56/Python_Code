062b960f575bc3aa8d9246b3ef1d1915700a7c8d
Samiul islam sami <samiulislam07186@gmail.com>
2026-07-19 17:46:15 +0600

    Add files via upload

---
+++ Squence_type_data.py
@@ -0,0 +1,32 @@
+#List Type Data
+
+Li = ['Sami' , 'shehab' , 'Rayhan', 'Naeem']
+print(Li);
+print(type(Li));
+
+#Mutable Change
+
+Li = ['Sami' , 'shehab' , 'Rayhan', 'Naeem']
+Li[2] = 'Oshiur'
+print(Li)
+
+#Tuple Type Data
+
+tup = (10,12,45,60) #immutable
+print(tup)
+print(type(tup))
+
+tup1 = ('Sami' , 'shehab' , 'Rayhan', 'Naeem')
+print(tup1)
+
+#Range type data
+
+ran = range(6)
+print(ran)
+
+for i in ran:
+    print(i)
+
+
+
+
